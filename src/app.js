e602f67a5c95e0ab0035c5b416dbe4b699d235a6
1320265 <1320265@senati.pe>
2025-11-20 18:00:47 -0500

    backend

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const sequelize = require("./settings/db");
+const userRoutes = require("./routes/user.routes");
+
+const app = express();
+app.use(express.json());
+
+app.use("/u", userRoutes);
+
+sequelize.authenticate()
+    .then(() => console.log("Conectando a la DB"))
+    .catch(err => console.error("Error ", err));
+
+module.exports = app;
